76d729c2f18da19baf4c3fb914eeb266925fd20d
Claire Elliott <gelliott181@gmail.com>
2025-01-21 17:13:55 -0500

    Update app-2.jsx

--- app-2.jsx
+++ app-2.jsx
@@ -3,22 +3,24 @@ const { Subscription, interval } = rxjs;
 const { map } = rxjs.operators;
 
 const App2 = () => {
-  const [data, setData] = useState('0');
+  const [data, setData] = useState(0);
+  const [msg, setMsg] = useState();
+  
   const action = (form) => toast(form.get('msg'));
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(e)
-    toast(data);
+    toast(msg);
   }
   
   return (
     <div className="card-body">
       <h5 className="card-title">Counter</h5>
       <p className="card-text">Count: {data}</p>
-      <a href="#" className="btn btn-primary" onClick={() => setData(Number(data) + 1)} >Do something</a>
+      <a href="#" className="btn btn-primary" onClick={() => setData(data + 1)} >Do something</a>
 
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder="Write something" name="msg" onChange={(e) => setData(e.target.value)} />
+        <input type="text" placeholder="Write something" name="msg" onChange={(e) => setMsg(e.target.value)} />
         <button type="submit">Toast!</button>
       </form>
     </div>
