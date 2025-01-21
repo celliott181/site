5661673478489b5a190e7b483acb4925fdb5b874
Claire Elliott <gelliott181@gmail.com>
2025-01-21 13:47:07 -0500

    Create app-2.jsx

---
+++ app-2.jsx
@@ -0,0 +1,33 @@
+const { useEffect, useState } = React;
+const { Subscription, interval } = rxjs;
+const { map } = rxjs.operators;
+
+const data$ = interval(1000).pipe(
+  map(v => v.toString())
+);
+
+const App = () => {
+  const [data, setData] = useState('0');
+  useEffect(() => {
+    const subscription = new Subscription();
+
+    const sub = data$.subscribe({
+      next: v => setData(v),
+      complete: () => console.log('Data stream complete'),
+    });
+
+    subscription.add(sub);
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return (
+    <div class="card-body">
+      <h5 class="card-title">Counter</h5>
+      <p class="card-text">Count: {data}</p>
+      <a href="#" class="btn btn-primary" onClick={() => alert('yay')} >Do something</a>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app-2'));
