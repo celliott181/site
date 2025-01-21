6146d86d24407fe7a8932657da68623734618a19
Claire Elliott <gelliott181@gmail.com>
2025-01-21 10:12:18 -0500

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,32 @@
+const { useEffect, useState } = React;
+const { Subscription, interval } = rxjs;
+
+function data() {
+  const intervalInMs = 1000; // Emit data points every 1000ms (1 second)
+  return interval(intervalInMs);
+}
+
+function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const data$ = data();
+    const subscription = new Subscription();
+
+    const sub = data$.subscribe({
+      next: v => setData(v),
+      complete: () => console.log('Data stream complete'),
+    });
+
+    subscription.add(sub);
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return (
+    <div>
+      {data}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
